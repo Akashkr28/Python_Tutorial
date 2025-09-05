ba36f7afe110a7c5e5881d0d1cadb86a0cb5922d
Akashkr28 <email.akash2010@gmail.com>
2025-09-05 20:42:38 +0530

    Import in Python

---
+++ 06_chai_business/main.py
@@ -0,0 +1,9 @@
+# import recipes.flavor
+# print(recipes.flavor.ginger_chai())
+
+
+# from recipes.flavor import elaichi_chai, ginger_chai
+# print(elaichi_chai())
+
+
+from .recipes.flavor import elaichi_chai
