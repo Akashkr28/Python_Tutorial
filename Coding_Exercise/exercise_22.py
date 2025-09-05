64d218e1371b83484cebfdcb1a2640741a1fb4b2
Akashkr28 <email.akash2010@gmail.com>
2025-09-05 17:44:10 +0530

    Added exercise and function topic

---
+++ Coding_Exercise/exercise_22.py
@@ -0,0 +1,35 @@
+# Function Types
+# You're building a Function Behavior Analyzer to showcase different types of Python functions in action. Implement the following:
+
+# 1. Pure Function
+#   - Write a function pure_add(a, b) that takes two integers and returns their sum.
+#   - It should not rely on or modify any external state.
+
+# 2. Impure Function
+#   - Define a global variable counter.
+#   - Implement impure_increment() that increases the counter and returns its value.
+
+# 3. Recursive Function
+#   - Implement factorial_recursive(n) that returns the factorial of a given number using recursion.
+#   - Handle base case correctly (e.g., factorial_recursive(0) = 1).
+
+# 4. Lambda Function with map()
+# Write a function square_list(nums) that uses a lambda inside map() to return a new list with the squares of the numbers in the input list.
+
+def pure_add(a, b):
+    return a + b
+    
+def impure_increment():
+    global counter
+    counter += 1
+    return counter
+    
+def factorial_recursive(n):
+    if n == 0:
+        return 1
+    return n * factorial_recursive(n - 1)
+print(factorial_recursive(5))
+
+def square_list(nums):
+    return list(map(lambda x: x ** 2, nums))
+print(square_list([1, 2, 3, 4]))
