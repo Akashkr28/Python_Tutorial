c3bb7cfb57e5fed95ea5bf40fc72b1b8f6f19caf
Akashkr28 <email.akash2010@gmail.com>
2025-09-06 22:17:37 +0530

    added generators topics

---
+++ 08_generators/04_close_generator.py
@@ -0,0 +1,25 @@
+def local_chai():
+    yield "Masala Chai"
+    yield "Ginger Chai"
+
+def impoorted_chai():
+    yield "Matcha Chai"
+    yield "Oolong Chai"
+
+def full_menu():
+    yield from local_chai()
+    yield from impoorted_chai()
+
+for chai in full_menu():
+    print(chai)
+
+def chai_stall():
+    try:
+        while True:
+            order = yield "Waiting for chai order"
+    except:
+        print("Stall closed, No more chai")
+
+stall = chai_stall()
+print(next(stall))
+stall.close() # Close the generator
